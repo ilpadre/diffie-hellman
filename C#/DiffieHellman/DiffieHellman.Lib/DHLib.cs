691579d71e0b09a92b96af7f0a04c35e2aad6d21
ilpadre <ilpadre1953@gmail.com>
2019-04-05 22:16:20 -0400

    Initial project commit

---
+++ C#/DiffieHellman/DiffieHellman.Lib/DHLib.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DiffieHellman.Lib
+{
+    public class DHLib
+    {
+    }
+}
---
+++ C#/DiffieHellman/DiffieHellman.Tests/DHTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DiffieHellmanTests
+{
+    public class DHTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
