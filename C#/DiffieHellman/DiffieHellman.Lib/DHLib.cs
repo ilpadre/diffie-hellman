b1735b88300044172b61d48476f212c7b6bb2e2d
ilpadre <ilpadre1953@gmail.com>
2019-04-07 08:12:42 -0400

    IsPrime and finding primes

--- C#/DiffieHellman/DiffieHellman.Lib/DHLib.cs
+++ C#/DiffieHellman/DiffieHellman.Lib/DHLib.cs
@@ -1,29 +1,77 @@
 ﻿using System;
 using System.Numerics;
+using System.Security.Cryptography;
 
 namespace DiffieHellman.Lib
 {
     public class DHLib
     {
-        public BigInteger GenerateLargestPrimeNumberLessThanInput(BigInteger n)
-        {
+        private static readonly BigInteger ONE = 1;
+        private static readonly BigInteger TWO = 2;
+        private static readonly BigInteger MINUS_ONE = -1;
+        private static readonly int PRIME_LENGTH = 1000000;
 
-            return BigInteger.One;
+        public static BigInteger GenerateLargeRandomPrime()
+        {
+            BigInteger p = RandomInteger(); 
+            while (IsPrime(p) == false) 
+            {
+                Console.WriteLine();
+                p = RandomInteger();
+
+            }
+
+            return p;
         }
 
+        public static BigInteger RandomInteger() 
+        {
+            var rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[PRIME_LENGTH / 8];
+            rng.GetBytes(bytes);
 
+            BigInteger p = new BigInteger(bytes);
+
+            return p;
+        }
+
+        public static BigInteger FindLargestPrimeLTEInput(BigInteger n)
+        {
+            if (n < 2)
+            {
+                return -1;
+            }
+
+            if (IsPrime(n))
+            {
+                return n;
+            }
+
+            if (n % 2 == 0)
+            {
+                n--;
+            }
+
+            for (BigInteger i = n; i > 0; i--)
+            {
+                if (IsPrime(i))
+                {
+                    return i;
+                }
+            }
+
+            return 2;
+        }
+
+    
         public static bool IsPrime(BigInteger number)
         {
-            if (number == 1)
+            if (number <= ONE)
             {
                 return false;
             }
-            else if (number == 2)
-            {
-                return true;
-            }
 
-            for (int i = 3; (BigInteger.Pow(i,2)) <= number; i += 2)
+            for (BigInteger i = 2; (BigInteger.Pow(i,2)) <= number; i += 2)
             {
                 if ((number % i) == 0)
                 {
--- C#/DiffieHellman/DiffieHellman.Tests/DHTests.cs
+++ C#/DiffieHellman/DiffieHellman.Tests/DHTests.cs
@@ -12,21 +12,28 @@ namespace DiffieHellmanTests
         }
 
         [Test]
-        public void TestThatOneIsNotPrime()
+        public void IsPrime_GivenThatOneIsNotPrime_ReturnsFalse()
         {
             var IsOnePrime = DHLib.IsPrime(1);
             Assert.AreEqual(false, IsOnePrime);
         }
 
         [Test]
-        public void TestThatTwoIsPrime()
+        public void IsPrime_GivenThatTwoIsPrime_ReturnsTrue()
         {
             var IsTwoPrime = DHLib.IsPrime(2);
             Assert.AreEqual(true, IsTwoPrime);
         }
 
         [Test]
-        public void TestThatLargerNumberIsPrime()
+        public void IsPrime_GivenThatSevenIsPrime_ReturnsTrue()
+        {
+            var IsTwoPrime = DHLib.IsPrime(7);
+            Assert.AreEqual(true, IsTwoPrime);
+        }
+
+        [Test]
+        public void IsPrime_GivenThat9601IsPrime_ReturnsTrue()
         {
             BigInteger bigNum = 9601;
             var isBigNumPrime = DHLib.IsPrime(bigNum);
@@ -34,15 +41,15 @@ namespace DiffieHellmanTests
         }
 
         [Test]
-        public void TestThatLargerNumberIsNotPrime()
+        public void IsPrime_GivenThat9602IsNotPrime_ReturnsFalse()
         {
             BigInteger bigNum = 9602;
             var isBigNumPrime = DHLib.IsPrime(bigNum);
-            Assert.AreEqual(true, isBigNumPrime);
+            Assert.IsFalse(isBigNumPrime);
         }
 
         [Test]
-        public void TestThatMuchLargerNumberIsPrime()
+        public void IsPrime_GivenThat5915587277IsPrime_ReturnsTrue()
         {
             BigInteger bigNum = 5915587277;
             var isBigNumPrime = DHLib.IsPrime(bigNum);
@@ -50,11 +57,78 @@ namespace DiffieHellmanTests
         }
 
         [Test]
-        public void TestThatMuchLargerNumberIsNotPrime()
+        public void IsPrime_GivenThat5915587278IsNotPrime_ReturnsFalse()
         {
             BigInteger bigNum = 5915587278;
             var isBigNumPrime = DHLib.IsPrime(bigNum);
             Assert.AreEqual(false, isBigNumPrime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIs10_Returns7()
+        {
+            BigInteger input = 10;
+            BigInteger expected = 7;
+            var prime = DHLib.FindLargestPrimeLTEInput(input);
+            Assert.IsTrue(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIs1_ReturnsMinus1()
+        {
+            BigInteger input = 1;
+            BigInteger expected = -1;
+            var prime = DHLib.FindLargestPrimeLTEInput(1);
+            Assert.IsFalse(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIsNegative_ReturnsMinus1()
+        {
+            BigInteger input = -7;
+            BigInteger expected = -1;
+            var prime = DHLib.FindLargestPrimeLTEInput(input);
+            Assert.IsFalse(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIsPrime_ReturnsInput()
+        {
+            BigInteger input = 5915587277;
+            BigInteger expected = 5915587277;
+            var prime = DHLib.FindLargestPrimeLTEInput(input);
+            Assert.IsTrue(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIs7432_Returns7417()
+        {
+            BigInteger input = 7432;
+            BigInteger expected = 7417;
+            var prime = DHLib.FindLargestPrimeLTEInput(input);
+            Assert.IsTrue(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void FindLargestPrimeLessThanInput_GivenThatInputIs1979339338_Returns1000000801()
+        {
+            BigInteger input = 1979339338;
+            BigInteger expected = 1979339333;
+            var prime = DHLib.FindLargestPrimeLTEInput(input);
+            Assert.IsTrue(DHLib.IsPrime(prime));
+            Assert.AreEqual(expected, prime);
+        }
+
+        [Test]
+        public void GenerateLargeRandomPrime_Executes_ReturnsLargeRandomPrime()
+        {
+            BigInteger p = DHLib.GenerateLargeRandomPrime();
+            Assert.IsTrue(DHLib.IsPrime(p));
         }
     }
 }
